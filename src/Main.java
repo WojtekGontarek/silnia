eefec5fa74f5a5e165229298635cb8c61160826e
C04H03 <C04H03@teb.edu.pl>
2024-04-08 12:15:02 +0200

    sdaf

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+    public static int silnia(int liczbaDoSilni){
+
+        try {
+
+        }
+
+        for (int i = liczbaDoSilni; i > 0)
+
+    }
+}
